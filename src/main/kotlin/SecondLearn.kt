46f134b23011cc756ead5803bc3d24cd8e17b0de
smit0101 <smitkalkani9099@gmail.com>
2022-06-30 19:56:09 +0530

    Tried Some Experiments With New Features

--- src/main/kotlin/SecondLearn.kt
+++ src/main/kotlin/SecondLearn.kt
@@ -1,8 +1,14 @@
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main(){
-
+    runBlocking {
+        GlobalScope.launch{
+            runBlocking {
+                println(Thread.currentThread().name)
+            }
+        }
+        println("K"+Thread.currentThread().name)
+    }
 }
